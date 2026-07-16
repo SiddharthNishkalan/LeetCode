a21727708366e0ffc8b08968f63febd714a4d35a
SiddharthNishkalan <s.siddharthnishkalan@gmail.com>
2026-07-16 19:57:32 +0530

    LC-valid-perfect-square [Runtime: N/A; Memory: N/A]

--- math/valid-perfect-square.cpp
+++ math/valid-perfect-square.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for(long long i=0;i<=num;i++){
-            if(i*i==num){
-                return true;
+        int start=1;
+        int end=num;
+        while(start<=end){
+            long long mid=start+(end-start)/2;
+            long long s=mid*mid;
+            if(sqr==num) return true;
+            else if(sqr<num){
+                start=mid+1
             }
+            else{
+                end=mid-1;
+            }
+
         }
         return false;
     }
