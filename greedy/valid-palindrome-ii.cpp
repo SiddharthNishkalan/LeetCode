915aa0835740d5a5e8776ce186ad701e03acaa5f
SiddharthNishkalan <s.siddharthnishkalan@gmail.com>
2026-07-22 19:21:15 +0530

    LC-valid-palindrome-ii [Runtime: N/A; Memory: N/A]

---
+++ greedy/valid-palindrome-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool palindrome(string&s,int right,int left){
+        while(left<right){
+            if(s[right]!=s[left]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s[right]!=s[left]){
+                return palindrome(s , right - 1, left) || palindrome(s ,  right, left + 1);
+        }
+        left++;
+        right--;}
+        return true;
+    }
+};
