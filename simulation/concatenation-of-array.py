64248f7c3fa5a85c5bfb2b1de52af19668fc03ec
SiddharthNishkalan <s.siddharthnishkalan@gmail.com>
2026-07-23 09:04:49 +0530

    LC-concatenation-of-array [Runtime: N/A; Memory: N/A]

--- simulation/concatenation-of-array.py
+++ simulation/concatenation-of-array.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def getConcatenation(self, nums):
-        
         return nums+nums
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+       
         
