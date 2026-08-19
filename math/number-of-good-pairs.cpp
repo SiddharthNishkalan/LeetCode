59530c77da68595a4835c1bef35438a2ee375d04
SiddharthNishkalan <s.siddharthnishkalan@gmail.com>
2026-08-19 18:41:58 +0530

    LC-number-of-good-pairs [Runtime: N/A; Memory: N/A]

---
+++ math/number-of-good-pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int res=0;
+        unordered_map<int,int> count;
+        for(int i:nums){
+            res+=count[i]++;
+        }
+        return res;
+    }
+};
