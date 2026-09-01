3d52b7b9acfe53b768148340e1599e491efd74f6
SiddharthNishkalan <s.siddharthnishkalan@gmail.com>
2026-09-01 18:32:56 +0530

    LC-factorial-trailing-zeroes [Runtime: N/A; Memory: N/A]

--- math/factorial-trailing-zeroes.cpp
+++ math/factorial-trailing-zeroes.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    long long fact(int n){
-        if(n==0 || n==1){
-            return 1;
-        }
-        return n*fact(n-1);
-    }
     int trailingZeroes(int n) {
-        long long fa=fact(n);
-        if(fa<10) return 0;
         int count=0;
-        while(fa%10==0){
-            count++;
-            fa=fa/10;
+        while(n>0){
+            n/=5;
+            count+=n;
         }
         return count;
     }
