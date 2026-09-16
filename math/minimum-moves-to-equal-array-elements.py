0820b6d7908a23f544bd29d05ab0fd6788f73829
Siddharth Nishkalan S <s.siddharthnishkalan@gmail.com>
2026-09-16 19:06:07 +0530

    LC-minimum-moves-to-equal-array-elements [Runtime: N/A; Memory: N/A]

--- math/minimum-moves-to-equal-array-elements.py
+++ math/minimum-moves-to-equal-array-elements.py
@@ -1,8 +1,5 @@
 class Solution(object):
     def minMoves(self, nums):
         return sum(nums)-len(nums)*min(nums)
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        
         
